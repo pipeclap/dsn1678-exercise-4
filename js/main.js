23bed63022af157ec11fb2cde4a5fa8c9e14e295
pipeclap <pipeclap@gmail.com>
2014-10-07 09:15:33 -0400

    more work of dipper and scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var $win = $(window);
+var $sun = $('.sun');
+var $sunSection = $('.sun-section');
+
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 
-	console.log (scrollPos);
+	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px')
 });	
