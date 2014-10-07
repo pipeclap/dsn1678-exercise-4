3ff482d1b7f1ecc9d1f6fa974230a3384d889eef
pipeclap <pipeclap@gmail.com>
2014-10-07 10:14:00 -0400

    add a spaceship

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $spaceShip = $('.space-chip');
+var $orangeSection = $('.orange-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px')
+
 });	
 
 $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
 }, { offset: '50%' });
+
+$
